2a793fd3f6753f5ee368c045e1aa46235bb5214f
Steve Marinelli <8030664+scmarinelli@users.noreply.github.com>
2025-05-30 10:38:22 -0400

    Update memory manager with state tracking

--- Jarvis.Core/Memory/IMemoryManager.cs
+++ Jarvis.Core/Memory/IMemoryManager.cs
@@ -1,8 +1,11 @@
-﻿namespace Jarvis.Core.Memory;
+using Jarvis.Environment;
+
+namespace Jarvis.Core.Memory;
 
 public interface IMemoryManager
 {
     void StoreEpisode(Episode episode);
+    void UpdateCurrentState(EnvironmentState state);
     void Consolidate();
     void Decay();
     AgentMemory GetCurrentMemory();
